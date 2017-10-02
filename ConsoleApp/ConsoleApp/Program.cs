93feef1ad8a694019ffab85f1261c0a39f65ada6
Vladislav <vambrosevich@gmail.com>
2017-10-02 03:28:00 +0300

    Implement AsyncWebClient

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            var result = WebClientAsync.AwaitWebClient(new Uri("uri"), cts.Token);
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/WebClientAsync.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class WebClientAsync
+    {
+        public static async Task<string> AwaitWebClient(Uri uri, CancellationToken ct)
+        {
+            var wc = new System.Net.WebClient();
+            var tcs = new TaskCompletionSource<string>();
+
+            wc.DownloadStringCompleted += (s, e) => {
+                if (e.Cancelled)
+                {
+                    tcs.SetCanceled();
+                }
+                else if (e.Error != null)
+                {
+                    tcs.SetException(e.Error);
+                }
+                else
+                {
+                    tcs.SetResult(e.Result);
+                }
+            };
+
+            wc.DownloadStringAsync(uri);
+            ct.Register(() =>
+            {
+                // this callback will be executed when token is cancelled
+                tcs.TrySetCanceled();
+            });
+            var result = await tcs.Task;
+
+            return result;
+        }
+    }
+}
