b02b175c7f0ac4c52b99d5548dc37293a4b10cc2
Vladislav <vambrosevich@gmail.com>
2017-10-10 10:23:53 +0300

    Add winforms prj with deadlock

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var http = GetHttp().Result;
+            label1.Text = http.Split(' ')[0];
+        }
+
+        private async Task<string> GetHttp()
+        {
+            var client = new HttpClient();
+            //            var msg = await client.GetAsync("http://tut.by/").ConfigureAwait(false);
+            var msg = await client.GetAsync("http://tut.by/");
+
+            //            return await msg.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return await msg.Content.ReadAsStringAsync();
+        }
+    }
+}
